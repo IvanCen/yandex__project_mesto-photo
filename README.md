fd93abcd2fc6c03973fc366e188ee4806c0ba817
Ivan Senkov <ivansenkov.web@gmail.com>
2020-02-11 18:47:45 +0300

    add project

---
+++ README.md
@@ -0,0 +1,16 @@
+# Что это?
+Лендинг в рамках обучения Яндекс.Практикум.  
+Страницы сверстанны на чистом CSS с применением методологии БЭМ (именование классов).  
+Работа с макетом в Figma.  
+Верстка проходила код ревью.  
+
+## Демо 
+![Main page](https://github.com/IvanCen/yandex__project_learn-to-learn/blob/master/images/main_pic.png "Научится учиться")
+
+## Как открыть?
+Пройти по ссылке: https://ivancen.github.io/yandex__project_learn-to-learn/
+
+## Стек технологий
+HTML, CSS.
+
+
---
+++ js/class/PopupImage.js
@@ -0,0 +1,13 @@
+class PopupImage extends Popup {
+    constructor(paramPopup) {
+        super(paramPopup);
+        this.paramPopup.root.addEventListener('click', this.openWindow.bind(this));
+    }
+    openWindow(element) {
+        if (element.target.classList.contains('place-card__image')) {
+            this.paramPopup.popupImage.classList.add('popup__image_is-opened');
+            this.paramPopup.popup.classList.add('popup_is-opened');
+            this.paramPopup.popupImage.style.backgroundImage = element.target.style.backgroundImage;
+        }
+    }
+}
